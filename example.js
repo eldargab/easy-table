9020abc647eddfde7aaca54e5ba87c2150ea1be7
Eldar Gabdullin <eldargab@gmail.com>
2012-07-23 16:40:02 +0400

    Refactoring

--- example.js
+++ example.js
@@ -11,15 +11,11 @@ var t = new Table
 data.forEach(function (product) {
     t.cell('Product Id', product.id)
     t.cell('Description', product.desc)
-    t.cell('Price, USD', product.price, function pricePrinter (price, width) {
-    	return Table.padLeft(price.toFixed(2), width)
-    })
+    t.cell('Price, USD', product.price, Table.Number(2))
     t.newRow()
 })
 
 t.sort(['Price, USD'])
-t.total('Price, USD', Table.aggr.avg, function (val, width) {
-    return Table.padLeft('Avg: ' + val.toFixed(2), width)
-})
+t.total('Price, USD', Table.aggr.avg, Table.aggr.Printer('Avg:', Table.Number(2)))
 
 console.log(t.toString())
